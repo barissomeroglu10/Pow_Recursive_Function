fc36145082e4de20f7173775c86eca5f0ef56df7
baris <baris@BARIS_SOMEROGLU>
2024-04-21 16:20:38 +0300

    Add project files.

---
+++ PowRecursiveFunction/PowWithRecursiveFunction.cpp
@@ -0,0 +1,44 @@
+﻿/*
+In this code, we see pow function with recursive method.
+
+Developer = Barış Someroğlu
+Date = 21.04.2024 - 04:10 pm
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+// Prototype for function
+int Pow(int, int);
+
+int main()
+{
+	int Flor, Base;
+
+	cout << "Please Enter Flor Number = ";
+	cin >> Flor;
+
+	cout << "Please Enter Base Number = ";
+	cin >> Base;
+
+	cout << "\n";
+
+	cout << "Result is = " << Pow(Flor, Base) << endl; // Call the function
+
+	return 0;
+}
+
+// Function definition
+int Pow(int Flor, int Base)
+{
+	if (Base < 0)
+		return -1;
+
+	else if (Base == 0)
+		return 1;
+
+	else
+		return Flor * Pow(Flor, Base - 1);
+}
